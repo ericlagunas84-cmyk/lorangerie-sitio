a5d486adcefb5c2b8b0d3e5c3f003e5b386499d6
ericlagunas84-cmyk <eric.lagunas84@gmail.com>
2026-07-19 01:15:28 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,9 +14,9 @@ const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
 ];
 
 self.addEventListener('install', (event) => {
